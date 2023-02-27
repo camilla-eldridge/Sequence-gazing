ef43469caeae2b450ad7f088ee0f11a81dc6b6c4
Camilla Eldridge <camillaeldridge@Monty.local>
2023-02-26 23:56:38 -0300

    add python examples

---
+++ Example_Pyscripts/examples_python.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 26 22:01:38 2023
+
+@author: yourname
+"""
+
+print("hello, world!")
+
+im_a_list = ["a", "b", "c"]
+print(im_a_list)
+
+im_a_tuple = ("a", "b", "c")
+print(im_a_tuple)
+
+dictionary = {'a': 'is for aardvark', 'b': 'is for beetle', 'c': 'is for camel'}
+print(dictionary)
+
+im_a_set = set("abbccc")
+print(im_a_set)
+
+im_a_string = "abc"
+print(im_a_string)
+
+im_also_a_string = 'abc'
+print(im_also_a_string)
