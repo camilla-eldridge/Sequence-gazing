9281251848625f89dd65380c7fd27bfe0d0b3090
Camilla Eldridge <k1116370@kingston.ac.uk>
2024-04-30 00:28:53 +0100

    Update Fib_example.py
    Changed index and made Fib sequence start at 1

--- Example_Pyscripts/Fib_example.py
+++ Example_Pyscripts/Fib_example.py
@@ -9,19 +9,23 @@ number=sys.argv[1]
 
 def fibonacci(n):
     fib_seq = np.zeros(n, dtype=int)
+    fib_seq[0] = 1
     fib_seq[1] = 1
     for i in range(2, n):
         fib_seq[i] = fib_seq[i-1] + fib_seq[i-2]
     return fib_seq
 
-# Specify the length of Fibonacci sequence (arg[1])
+# Specify the length of Fib sequence (arg[1])
 number=int(number)
 
-# Generate Fibonacci sequence
+# Generate Fib sequence
 sequence=fibonacci(number)
 
-# Create a pandas dataframe to store and index the Fibonacci sequence
+# Create a pandas dataframe to store and index Fib sequence
 sequence_df = pd.DataFrame({'Index': range(number), 'Fibonacci Number': sequence})
+
+# Make the index more human readible (add 1 to index column)
+sequence_df['Index'] += 1
 
 # print the dataframe 
 print(sequence_df[1:])
